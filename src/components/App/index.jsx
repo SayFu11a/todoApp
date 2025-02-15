2f52f0f505973209e6af44b344fe935ae5090d6e
Sayfu11a <sgishlakaev@gmail.com>
2025-02-15 22:00:09 +0300

    interactive

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,19 +1,44 @@
+import { Component } from "react";
+
 import Header from "../Header"
 import TaskList from "../TaskList"
 import Footer from '../Footer'
 
 import './App.css'
 
-const App = () => {
-    return (
-        <section className="todoapp">
-          <Header />
-          <section className="main">
-            <TaskList />
-            <Footer />
-          </section>
-        </section>
-    )
-}
+export default class App extends Component {
 
-export default App
+    state = {
+        todosArr: [
+            { label: 'Drink Coffee',  time: 'created 17 seconds ago', id: 1 },
+            { label: 'Drink Coffee2', time: 'created 37 seconds ago', id: 2 },
+            { label: 'Drink 333', time: 'created 57 seconds ago', id: 3 },
+        ]
+    }
+
+    deleteItem = (id) => {
+        this.setState(({todosArr}) => {
+            const idx = todosArr.findIndex(el => el.id === id)
+            const before = todosArr.slice(0, idx)
+            const after = todosArr.slice(idx + 1)
+
+            return {
+                todosArr: [...before, ...after]
+            }
+        })
+    }
+
+    render () {
+        return (
+            <section className="todoapp">
+                <Header />
+                <section className="main">
+                    <TaskList 
+                        todosArr={ this.state.todosArr }
+                        onDeleted={ this.deleteItem }/>
+                    <Footer />
+                </section>
+            </section>
+        )
+    }
+}
--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,19 +1,51 @@
+import { Component } from "react"
+
+export default class Task extends Component {
+
+    state = {
+        complited: false,
+    }
+
+    onLiClick = () => {
+        this.setState( (state) => {
+            return { 
+                complited: !state.complited
+            }
+        })
+    }
+
+    onMarkImportant = () => {
+        this.setState((state) =>{
+            return {
+                important: !state.important
+            }
+        })
+    }
 
 
-const Task = ({label, complited, time}) => {
-  return (
-    <li>
-    <div className="view">
-      <input className="toggle" type="checkbox" />
-      <label>
-        <span className="description">{label}</span>
-        <span className="created">{time}</span>
-      </label>
-      <button className="icon icon-edit"></button>
-      <button className="icon icon-destroy"></button>
-    </div>
-  </li>
-  )
+    render () {
+        const { label, time, onDeleted } = this.props
+        const { complited } = this.state
+
+
+        return (
+            <li className={ complited ? 'completed' : ''}>
+            <div className="view">
+            <input 
+                className="toggle" 
+                type="checkbox" 
+                checked={complited} 
+                onChange={ this.onLiClick } 
+            />
+            <label onClick={ this.onLiClick } >
+                <span className="description">{label}</span>
+                <span className="created">{time}</span>
+            </label>
+            <button className="icon icon-edit"></button>
+            <button 
+                onClick={ onDeleted }
+                className="icon icon-destroy"></button>
+            </div>
+        </li>
+    )}
 }
-
-export default Task
